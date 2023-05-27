f98a19c7027c80336840cc845ebeaa95e7f3558b
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-05-27 15:28:49 +0300

    Style NavBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+       <img src="https://www.freepnglogos.com/uploads/netflix-logo-history-png-33.png" alt="netflix logo" />
+
+    </div>
   )
 }
 
