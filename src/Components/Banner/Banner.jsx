ce98f1df8a14380cde2c343d17b75ee4f23e9b71
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-05-27 13:20:38 +0300

    styled the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,19 +8,29 @@ function Banner() {
     useEffect(() => {
         async function fetchData() {
             const requests = await axios.get(request.fetchNetflixOriginals);
-            setMovie(requests.data.results[Math.floor(Math.random())])
+            setMovie(requests.data.results[Math.floor(Math.random() * requests.data.results.length - 1)]);
+            return requests;
         }
         fetchData();
     }, []);
+
+    console.log(movie)
   return (
-      <header>
+      <header className='banner'
+          style={{
+              backgroundSize: "cover",
+              backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+              backgroundPosition: "center center",
+      }}
+      >
+          <div className="banner__contents">
           {/* - background image
           - the title
            - div for  2 buttons(play and mylist)
            - Description*/}
-          
+          </div>
     </header>
   )
 }
 
-export default Banner
+export default Banner;
